b695091296c234b0d9de1be28262030c2944e650
LethalMaus <descriptiveanimals@gmail.com>
2023-10-19 13:47:44 +0200

    Updated testimonials and its handling

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
-const testimonialText = document.getElementById("testimonial-text");
+const testimonialContainer = document.querySelector(".testimonial-container");
+const testimonial = document.querySelector(".testimonial");
+const testimonialContent = document.querySelector(".testimonial-content");
+const testimonialImage = document.getElementById("testimonial-image");
 const testimonialAuthor = document.getElementById("testimonial-author");
 const testimonialCompany = document.getElementById("testimonial-company");
-const testimonialImage = document.getElementById("testimonial-image");
-const testimonial = document.querySelector(".testimonial");
-const testimonialContainer = document.querySelector(".testimonial-container");
+const testimonialText = document.getElementById("testimonial-text");
+const testimonialLinks = document.getElementById("testimonial-links");
+const linkedinLink = document.getElementById("testimonial-linkedin-link");
+const websiteLink = document.getElementById("testimonial-website-link");
 const prevButton = document.getElementById("prev-button");
 const nextButton = document.getElementById("next-button");
 
@@ -12,6 +16,20 @@ fetch("testimonials.json")
     .then(data => {
         let index = 0;
         let animationTimeout;
+		
+		testimonialContent.addEventListener("click", function() {
+			if (testimonialContainer.classList.contains('enlarge') === true) {
+				testimonialContainer.classList.remove('enlarge')
+				testimonialText.style.webkitBoxOrient = 'vertical'
+				testimonialLinks.style.display = 'none';
+			} else {
+				testimonialContainer.classList.add('enlarge')
+				testimonialText.style.webkitBoxOrient = 'unset'
+				testimonialLinks.style.display = 'block';
+			}
+			setMaxLines();
+		});
+		
 
         function displayTestimonial() {
             animationTimeout = setTimeout(() => {
@@ -20,15 +38,27 @@ fetch("testimonials.json")
                 testimonialAuthor.textContent = data[index].author;
                 testimonialCompany.textContent = data[index].company;
 				testimonial.style.animationName = "fadeIn";
-				let link = data[index].link
-				testimonial.addEventListener("click", function() {
-					window.open(link, '_blank').focus();
-				});
+				let linkedin = data[index].linkedin
+				if (linkedin) {
+					linkedinLink.onclick = function() {
+						window.open(linkedin, '_blank').focus();
+					}
+					linkedinLink.style.display = null
+				} else {
+					linkedinLink.style.display = 'none'
+				}
+				let website = data[index].website
+				if (website) {
+					websiteLink.onclick = function() {
+						window.open(website, '_blank').focus();
+					}
+					websiteLink.style.display = null
+				} else {
+					websiteLink.style.display = 'none'
+				}
 				index = (index + 1) % data.length;
 				testimonialContainer.style.display = "flex";
-				const containerHeight = testimonialContainer.clientHeight;
-				const maxLines = Math.floor(containerHeight / 35);
-				testimonialText.style.webkitLineClamp = maxLines;
+				setMaxLines();
             }, 500);
         }
 
@@ -53,3 +83,9 @@ fetch("testimonials.json")
             displayTestimonial();
         });
     });
+	
+function setMaxLines() {
+	const containerHeight = testimonialContainer.clientHeight;
+	const maxLines = Math.floor(containerHeight / 35);
+	testimonialText.style.webkitLineClamp = maxLines;
+}
